aa2127b43773be558b066365391ab276315d2820
Rokas Kasperavicius <goodname258@gmail.com>
2022-02-26 18:33:43 +0100

    Added rectangle shape class

---
+++ src/com/company/Rectangle.java
@@ -0,0 +1,49 @@
+package com.company;
+
+// Same as the Java Point class but accepts double values
+import java.awt.geom.Point2D;
+import java.awt.Point;
+
+public class Rectangle extends Shape {
+  final private int width;
+  final private int height;
+
+  final private Point bottomLeftPoint;
+
+  public Rectangle(int width, int height, Point point) {
+    this.width = width;
+    this.height = height;
+
+    this.bottomLeftPoint = point;
+  }
+
+  public Point2D getCenter() {
+    // Calculate center coordinates with added bottom left point offset
+    double x = (double)this.width / 2 + this.bottomLeftPoint.getX();
+    double y = (double)this.height / 2 + this.bottomLeftPoint.getY();
+
+    return new Point2D.Double(x, y);
+  }
+
+  public int getArea() {
+    return this.width * this.height;
+  }
+
+  public int getCircumference() {
+    return this.width * 2 + this.height * 2;
+  }
+
+  public boolean isPointInside(Point point) {
+    int x = point.x;
+    int y = point.y;
+
+    int shapeX = this.bottomLeftPoint.x;
+    int shapeY = this.bottomLeftPoint.y;
+
+    return (x >= shapeX && y >= shapeY && x <= this.width + shapeX && y <= this.height + shapeY);
+  }
+
+  public double getEuclideanDistance(Shape shape) {
+    return this.getCenter().distance(shape.getCenter());
+  }
+}
