d54c7622cea007c543a37956dfe2fe991a1ee435
Rokas Kasperavicius <goodname258@gmail.com>
2022-02-26 23:50:26 +0100

    created circle class

---
+++ src/com/company/Circle.java
@@ -0,0 +1,33 @@
+package com.company;
+
+// Same as the Java Point class but accepts double values
+import java.awt.geom.Point2D;
+import java.awt.Point;
+
+public class Circle extends Shape {
+  final private Point center;
+  final private int radius;
+
+  public Circle(Point center, int radius) {
+    this.center = center;
+    this.radius = radius;
+  }
+
+  public Point2D getCenter() {
+    return this.center;
+  }
+
+  public double getArea() {
+    return Math.pow(this.radius, 2) * Math.PI;
+  }
+
+  public double getCircumference() {
+    return 2 * this.radius * Math.PI;
+  }
+
+  // https://stackoverflow.com/questions/481144/equation-for-testing-if-a-point-is-inside-a-circle
+  public boolean isPointInside(Point point) {
+    return Math.pow(point.x - center.x, 2) + Math.pow(point.y - center.y, 2)  <= radius * radius;
+  }
+}
+
--- src/com/company/ShapeTest.java
+++ src/com/company/ShapeTest.java
@@ -16,6 +16,9 @@ public class ShapeTest {
   // Defining class objects for testing triangle class
   Triangle triangle = new Triangle(new Point(), new Point(3, 9), new Point(6, 0));
 
+  // Defining class objects for testing circle class
+  Circle circle = new Circle(new Point(), 5);
+
   @Test
   public void testGetCenter() {
     // Rectangle
@@ -24,6 +27,9 @@ public class ShapeTest {
 
     // Triangle
     assertEquals(triangle.getCenter(), new Point2D.Double(3.0, 3.0));
+
+    // Circle
+    assertEquals(circle.getCenter(), new Point2D.Double(0.0, 0.0));
   }
 
   @Test
@@ -34,6 +40,9 @@ public class ShapeTest {
 
     // Triangle
     assertEquals(triangle.getArea(), 27);
+
+    // Circle
+    assertEquals(circle.getArea(), 78.53981633974483);
   }
 
   @Test
@@ -44,6 +53,9 @@ public class ShapeTest {
 
     // Triangle
     assertEquals(triangle.getCircumference(), 24.973665961010276);
+
+    // Circle
+    assertEquals(circle.getCircumference(), 31.41592653589793);
   }
 
   @Test
@@ -67,14 +79,28 @@ public class ShapeTest {
 
     assertFalse(triangle.isPointInside(new Point(7, 0)));
     assertFalse(triangle.isPointInside(new Point(1, 4)));
+
+    // Circle
+    assertTrue(circle.isPointInside(new Point(-1, -3)));
+    assertTrue(circle.isPointInside(new Point(-5, 0)));
+    assertTrue(circle.isPointInside(new Point(3, 2)));
+
+    assertFalse(circle.isPointInside(new Point(6, 0)));
+    assertFalse(circle.isPointInside(new Point(-5, 4)));
   }
 
   @Test
   // Euclidean distance is calculated using Java Point2D class method, therefore, there is no need for in-depth testing
   public void testGetEuclideanDistance() {
+    // Rectangle
     assertEquals(rectangle.getEuclideanDistance(rectangleMoved), 5.0);
     assertEquals(rectangle.getEuclideanDistance(rectangleOffset), 2.23606797749979);
 
+    // Triangle
     assertEquals(triangle.getEuclideanDistance(triangle), 0.0);
+
+    // Circle
+    assertEquals(circle.getEuclideanDistance(rectangle), 5.5901699437494745);
+    assertEquals(circle.getEuclideanDistance(circle), 0.0);
   }
 }
